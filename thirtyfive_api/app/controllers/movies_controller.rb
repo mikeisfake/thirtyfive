723e43432b6f744de890fad92e9c3a233e0fad88
mikeisfake <mikescooper@icloud.com>
2021-01-26 17:46:33 -0500

    begin stubbing out movies controller actions

--- thirtyfive_api/app/controllers/movies_controller.rb
+++ thirtyfive_api/app/controllers/movies_controller.rb
@@ -1,2 +1,17 @@
 class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+    render json: @movies
+  end
+
+  def show
+    @movie = Movie.find_by(id: params[:id])
+    render json:@movie
+  end
+
+  def create
+
+  end
+
 end
