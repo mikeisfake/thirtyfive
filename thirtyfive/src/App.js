69d20bdbf552bc4d409536a3c7dc00208e2bc0cc
mikeisfake <mikescooper@icloud.com>
2021-01-29 07:23:16 -0500

    begin adding some routes

--- thirtyfive/src/App.js
+++ thirtyfive/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import NavContainer from './containers/NavContainer'
+import ReviewForm from './containers/ReviewForm'
 
 function App() {
   return (
     <div className="App">
-      <NavContainer />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={NavContainer} />
+          <Route exact path="review/new" component={ReviewForm} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- thirtyfive/src/index.js
+++ thirtyfive/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { applyMiddleware, createStore, compose } from 'redux';
 import { reducer } from './redux/reducer'
 import logger from 'redux-logger';
@@ -10,8 +9,6 @@ import { Provider } from 'react-redux'
 require('dotenv').config()
 
 const store = createStore(reducer)
-
-
 
 ReactDOM.render(
   <Provider store={store}>
